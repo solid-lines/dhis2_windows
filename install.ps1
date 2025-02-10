bb126da23719f4639d31844576515eeb1e3e87a1
Daniel Castelao Suárez <48926694+dcastelaosua@users.noreply.github.com>
2025-02-10 12:36:13 +0100

    Added first release

---
+++ install.ps1
@@ -0,0 +1,114 @@
+#######################
+# Functions
+#######################
+
+# Add firewall rule if it does not exist REVIEW
+function Add-PortFirewall {
+    param (
+        [int]$Port
+    )
+
+    if (Get-NetFirewallRule | Where-Object {($_ | Get-NetFirewallPortFilter).LocalPort -eq ${Port} -and $_.Enabled -eq "True"}) {
+		Write-Host "Port ${Port} already opened in the firewall"
+	} else {
+		Write-Host "Adding firewall rule to allow port ${Port}"
+		New-NetFirewallRule -DisplayName "Allow Port ${Port}" -Direction Inbound -Protocol "TCP" -LocalPort ${Port}.ToString() -Action Allow
+	}
+}
+
+#######################
+# Script
+#######################
+
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "DHIS2 has to be installed as Administrator."
+    Exit 1
+}
+
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+Write-Host "Init DHIS2 installation...."
+Write-Host "Loading config settings"
+
+# Load config.json and get variables
+$config = Get-Content -Raw -Path ".\config.json" | ConvertFrom-Json
+
+$jdk_version = $config.jdk.version
+$tomcat_version = $config.tomcat.version
+$tomcat_service_name = $config.tomcat.service_name
+$tomcat_xmx = $config.tomcat.xmx
+$tomcat_xms = $config.tomcat.xms
+$tomcat_username = $config.tomcat.username
+$tomcat_password = $config.tomcat.password
+$glowroot_enabled = $config.monitoring.glowroot.enabled
+$glowroot_version = $config.monitoring.glowroot.version
+$glowroot_username = $config.monitoring.glowroot.username
+$glowroot_password = $config.monitoring.glowroot.password
+$prometheus_grafana_enabled = $config.monitoring.prometheus_grafana.enabled
+$prometheus_version = $config.monitoring.prometheus_grafana.prometheus_version
+$grafana_version = $config.monitoring.prometheus_grafana.grafana_version
+$windows_exporter_version = $config.monitoring.prometheus_grafana.windows_exporter_version
+$pg_version = $config.postgresql.version
+$pg_host = $config.postgresql.host
+$pg_port = $config.postgresql.port
+$pg_username = $config.postgresql.username
+$pg_password = $config.postgresql.password
+$pg_service_name = $config.postgresql.service_name
+$pg_max_connections = $config.postgresql.max_connections
+$pg_memory = $config.postgresql.memory
+$pg_cpus = $config.postgresql.cpus
+$postgis_version = $config.postgresql.postgis_version
+$dhis2_version = $config.dhis2.version
+$dhis2_home = $config.dhis2.home
+$dhis2_path = $config.dhis2.path
+$dhis2_db_name = $config.dhis2.db_name
+$dhis2_db_username = $config.dhis2.db_username
+$dhis2_db_password = $config.dhis2.db_password
+$proxy_version = $config.proxy.version
+$proxy_hostname = $config.proxy.hostname
+$proxy_service_name = $config.proxy.service_name
+
+
+# If hostname is not localhost, create firewall rules to open ports 80 and 443
+if (${proxy_hostname} -ne "localhost") {
+	Write-Host "Adding firewall rules to allow HTTP and HTTPS connections."
+	Add-PortFirewall -Port 80
+	Add-PortFirewall -Port 443
+} else {
+	Write-Host "Hostname is localhost. No firewall rules needed."
+}
+
+# Install JDK
+try {
+    .\install_OpenJDK.ps1 -jdk_version $jdk_version
+} catch {
+    Write-Error "JDK $jdk_version installation failed: $_"
+}
+
+# Install Tomcat and Glowroot
+try {
+    .\install_Tomcat.ps1 -tomcat_version $tomcat_version -tomcat_service_name $tomcat_service_name -tomcat_xmx $tomcat_xmx -tomcat_xms $tomcat_xms -tomcat_username $tomcat_username -tomcat_password $tomcat_password -glowroot_version $glowroot_version -glowroot_username $glowroot_username -glowroot_password $glowroot_password
+} catch {
+    Write-Error "Tomcat $tomcat_version installation failed: $_"
+}
+
+# Install PostgreSQL, postgis and PGAdmin
+try {
+    .\install_PostgreSQL.ps1 -pg_version $pg_version -pg_username $pg_username -pg_password $pg_password -pg_port $pg_port -pg_service_name $pg_service_name -pg_max_connections $pg_max_connections -pg_memory $pg_memory -pg_cpus $pg_cpus -dhis2_db_name $dhis2_db_name -postgis_version $postgis_version
+} catch {
+    Write-Error "PostgreSQL $pg_version installation failed: $_"
+}
+
+# Install DHIS2
+try {
+    .\install_DHIS2.ps1 -dhis2_version $dhis2_version -dhis2_path $dhis2_path -dhis2_db_name $dhis2_db_name -dhis2_db_username $dhis2_db_username -dhis2_db_password $dhis2_db_password -dhis2_home $dhis2_home -pg_version $pg_version -pg_host $pg_host -pg_port $pg_port -pg_username $pg_username -pg_password $pg_password -pg_service_name $pg_service_name -tomcat_service_name $tomcat_service_name -tomcat_version $tomcat_version -proxy_hostname $proxy_hostname
+} catch {
+    Write-Error "DHIS2 $dhis2_version installation failed: $_"
+}
+
+# Install Nginx
+try {
+    .\install_Nginx.ps1 -proxy_hostname $proxy_hostname -proxy_version $proxy_version -proxy_service_name $proxy_service_name
+} catch {
+    Write-Error "Nginx installation failed: $_"
+}
---
+++ install_Nginx.ps1
@@ -0,0 +1,276 @@
+#######################
+# Params
+#######################
+param (
+    [string]$proxy_hostname,
+    [string]$proxy_version,
+	[string]$proxy_service_name
+)
+
+$nginx_install_path = "C:\Program Files\Nginx"
+$letsencrypt_dir = "C:\Certbot"
+
+#######################
+# Functions
+#######################
+
+# Download and install nginx
+function Download-Install-Nginx {
+	Write-Host "Downloading and installing Nginx v${proxy_version}..."
+	$nginx_url = "https://nginx.org/download/nginx-${proxy_version}.zip"
+	$nginx_zip = ".\nginx.zip"
+	Invoke-WebRequest -Uri ${nginx_url} -OutFile ${nginx_zip} -UseBasicParsing
+	
+	# Remove the previous nginx folder and extract the installation zip file
+	if (Test-Path -Path ${nginx_install_path}) {
+		Remove-Item -Path ${nginx_install_path} -Recurse -Force
+	}
+	Expand-Archive -Path ${nginx_zip} -DestinationPath ${nginx_install_path} -Force
+	#Remove-Item -Path $nginx_zip
+}
+
+# Preconfigure Nginx creating performance.conf, gzip.conf, security.conf, stub-status.conf, proxycommon.conf, proxysecurity.conf and nginx.conf configuration files
+function Preconfigure-Nginx {
+	Write-Host "Configuring Nginx..."
+	$nginx_performance_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\performance.conf"
+	$nginx_gzip_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\gzip.conf"
+	$nginx_security_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\security.conf"
+	$nginx_proxycommon_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\proxycommon.conf"
+	$nginx_proxysecurity_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\proxysecurity.conf"
+	$nginx_stub_status_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\stub_status.conf"
+	$nginx_base_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\nginx.conf"
+
+	$nginx_stub_status_conf_content = @"
+server {
+	listen 127.0.0.1:80;
+	server_name 127.0.0.1;
+	location /nginx_status {
+		access_log off;
+		stub_status on;
+		allow 127.0.0.1;
+		deny all;
+	}
+}
+"@
+	Set-Content -Path $nginx_stub_status_conf_file -Value $nginx_stub_status_conf_content
+	
+	$nginx_proxycommon_conf_content = @"
+proxy_redirect            off;
+proxy_set_header Host `$host;
+proxy_set_header X-Real-IP `$remote_addr;
+proxy_set_header X-Forwarded-For `$proxy_add_x_forwarded_for;
+proxy_set_header X-Forwarded-Proto `$scheme;
+
+proxy_connect_timeout   480s;
+proxy_read_timeout      480s;
+proxy_send_timeout      480s;
+proxy_buffer_size       128k;
+proxy_buffers   8 128k;
+proxy_busy_buffers_size 256k;
+"@
+	Set-Content -Path $nginx_proxycommon_conf_file -Value $nginx_proxycommon_conf_content
+
+	$nginx_proxysecurity_conf_content = @"
+# Referrer Policy
+proxy_set_header Referrer-Policy "no-referrer";
+# Avoid clickjacking attack
+proxy_set_header X-Frame-Options "SAMEORIGIN";
+# Enable Strict Transport Security (HSTS) for https
+proxy_set_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload";
+# Protect against MIME sniffing vulnerabilities
+proxy_set_header X-Content-Type-Options "nosniff";
+# X-XSS Protection
+proxy_set_header X-XSS-Protection "1; mode=block";
+proxy_hide_header X-Powered-By;
+proxy_hide_header Server;
+"@
+	Set-Content -Path $nginx_proxysecurity_conf_file -Value $nginx_proxysecurity_conf_content
+	
+	$nginx_performance_conf_content = @"
+sendfile              on;
+tcp_nopush            on;
+tcp_nodelay           on;
+keepalive_timeout     10;
+send_timeout 10;
+types_hash_max_size   2048;
+client_max_body_size  100M;
+client_body_timeout 10;
+client_header_timeout 10;
+large_client_header_buffers 8 16k;
+"@
+	Set-Content -Path $nginx_performance_conf_file -Value $nginx_performance_conf_content
+	
+	$nginx_gzip_conf_content = @"
+gzip on;
+gzip_vary on;
+gzip_proxied any;
+gzip_comp_level 6;
+gzip_types text/plain text/css text/xml application/json application/javascript application/rss+xml application/atom+xml image/svg+xml;
+"@
+	Set-Content -Path $nginx_gzip_conf_file -Value $nginx_gzip_conf_content
+
+	$nginx_security_conf_content = @"
+# Hide nginx server version
+server_tokens off;
+"@
+	Set-Content -Path $nginx_security_conf_file -Value $nginx_security_conf_content
+	
+	$nginx_base_conf_content = @"
+worker_processes auto;
+error_log logs\error.log;
+
+events { worker_connections  1024; }
+
+http {
+	log_format  main  '`$remote_addr - `$remote_user [`$time_local] "`$request" '
+        '`$status `$body_bytes_sent "`$http_referer" '
+        '"`$http_user_agent" "`$http_x_forwarded_for"';
+        access_log logs\access.log;
+
+    include  mime.types;
+	include	 *.conf;
+    default_type  application/octet-stream;
+
+    server {
+        listen 80;
+        server_name ${proxy_hostname};
+		
+        location / {
+            proxy_pass http://localhost:8080/;
+            include proxycommon.conf;
+			include proxysecurity.conf;
+        }
+    }
+}
+"@
+	Set-Content -Path $nginx_base_conf_file -Value $nginx_base_conf_content
+}
+
+# Create Nginx Windows Service with nssm (https://nssm.cc/)
+function Create-Nginx-Service {
+	# Install nssm
+	$nssm_url = "https://nssm.cc/release/nssm-2.24.zip"
+	$nssm_file = ".\nssm.zip"
+	Invoke-WebRequest -Uri ${nssm_url} -OutFile ${nssm_file} -UseBasicParsing
+	Expand-Archive -Path ${nssm_file} -DestinationPath "C:\Program Files\" -Force
+	Remove-Item -Path ${nssm_file}
+	$current_path = Get-Location
+	Set-Location "C:\Program Files\nssm-2.24\win64\"
+	$nginx_exe = "${nginx_install_path}\nginx-${proxy_version}\nginx.exe"
+	cmd.exe /c ".\nssm.exe install ${proxy_service_name} `"${nginx_exe}`""
+	# Start-Service -Name ${proxy_service_name}
+	Set-Location ${current_path}
+}
+
+# Download and install Certbot to get SSL Certificates (REVIEW if localhost)
+function Install-Certbot {
+	Write-Host "Downloading and installing certbot to request SSL certificate..."
+	if (-not (Test-Path -Path $letsencrypt_dir)) {
+		New-Item -ItemType Directory -Path $letsencrypt_dir
+	}
+	
+	$certbot_url = "https://github.com/certbot/certbot/releases/download/v2.9.0/certbot-beta-installer-win_amd64_signed.exe"
+	$certbot_installer = ".\certbot-installer.exe"
+	Invoke-WebRequest -Uri $certbot_url -OutFile $certbot_installer
+	Start-Process -FilePath $certbot_installer -ArgumentList "/S" -Wait
+	Move-Item -Path "C:\Program Files\Certbot\*" -Destination ${letsencrypt_dir} -Force | Out-Null
+	[Environment]::SetEnvironmentVariable("PATH", $env:PATH + ";${letsencrypt_dir}", [EnvironmentVariableTarget]::Machine)
+	Remove-Item -Path $certbot_installer
+	
+	# Request SSL Certificate for the domain name
+	Write-Host "Requesting SSL Certificate..."
+	$certbot_exe = "$letsencrypt_dir\bin\certbot.exe"
+	Start-Process -FilePath $certbot_exe -ArgumentList "certonly --standalone -n --agree-tos -m admin@${proxy_hostname} -d ${proxy_hostname}" -Wait
+}
+
+# Configure Nginx with SSL
+function Configure-SSL-Nginx {
+	Write-Host "Configuring Nginx with SSL..."
+	$nginx_base_ssl_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\nginx.conf"
+	$nginx_ssl_conf_file = "${nginx_install_path}\nginx-${proxy_version}\conf\ssl.conf"
+	
+	$nginx_ssl_conf_content = @"
+# SSL settings
+ssl_protocols TLSv1.2 TLSv1.3;
+ssl_prefer_server_ciphers on;
+
+ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
+
+ssl_session_cache shared:SSL:20m;
+ssl_session_timeout 20m;
+ssl_session_tickets off;
+
+# SSL OCSP stapling
+ssl_stapling         on;
+ssl_stapling_verify  on;
+
+# DNS resolver configuration for OCSP response
+resolver          8.8.4.4 8.8.8.8 valid=300s ipv6=off;
+resolver_timeout  10s;
+"@
+	Set-Content -Path $nginx_ssl_conf_file -Value $nginx_ssl_conf_content
+	
+	$nginx_base_ssl_conf_content = @"
+worker_processes auto;
+error_log logs\error.log;
+
+events { worker_connections  1024; }
+
+http {
+	log_format  main  '`$remote_addr - `$remote_user [`$time_local] "`$request" '
+        '`$status `$body_bytes_sent "`$http_referer" '
+        '"`$http_user_agent" "`$http_x_forwarded_for"';
+        access_log logs\access.log;
+
+    include  mime.types;
+	include	 performance.conf;
+	include	 security.conf;
+	include  gzip.conf;
+	include  ssl.conf;
+    default_type  application/octet-stream;
+	
+	server {
+		listen  443 ssl;
+		server_name  ${proxy_hostname};
+	
+		ssl_certificate  C:\Certbot\live\${proxy_hostname}\fullchain.pem;
+		ssl_certificate_key  C:\Certbot\live\${proxy_hostname}\privkey.pem;
+	
+		location /glowroot {
+			proxy_pass http://localhost:4000/;
+            include proxycommon.conf;
+			include proxysecurity.conf;
+		}
+			
+		location / {
+			proxy_pass http://localhost:8080/;
+            include proxycommon.conf;
+			include proxysecurity.conf;
+		}
+	}
+	
+	server {
+		listen  80;
+		server_name  ${proxy_hostname};
+	
+		return 301 https://`$host\`$request_uri;
+	}
+}
+"@
+	Set-Content -Path $nginx_base_ssl_conf_file -Value $nginx_base_ssl_conf_content
+}
+
+#######################
+# Script
+#######################
+
+Write-Host "Init Nginx v${proxy_version} proxy server installation..."
+
+Download-Install-Nginx
+Preconfigure-Nginx
+Create-Nginx-Service
+Install-Certbot
+Configure-SSL-Nginx
+Restart-Service -Name ${proxy_service_name}
+
+Write-Host "Nginx installed and configured successfully as proxy server (https://${proxy_hostname})"
