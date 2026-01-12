2fa5f17b164a13d4604f6bf84a24a9b445c1484e
Daniel Castelao Suárez <daniel.castelao@solidlines.io>
2026-01-12 15:38:37 +0100

    Fix Grafana Dashboards

--- install.ps1
+++ install.ps1
@@ -3,6 +3,10 @@
 #######################
 
 function Init-Logs {
+	param(
+        [string]$Root_Location
+    )
+	
 	# Log Levels
 	$global:LogLevels = @{
 		"DEBUG" = 0
@@ -20,7 +24,7 @@ function Init-Logs {
 	}
 
 	# Create logs path
-	$global:LogPath = if ($config.logging.path) { $config.logging.path } else { ".\logs" }
+	$global:LogPath = if ($config.logging.path) { "${Root_Location}/$config.logging.path" } else { ".\logs" }
 	$global:LogFile = "$LogPath\install_$(Get-Date -Format 'yyyyMMdd_HHmmss').log"
 	New-Item -Path $LogPath -ItemType Directory -Force | Out-Null
 }
@@ -92,7 +96,8 @@ try {
     Exit 1
 }
 
-Init-Logs
+$Root_Location = Get-Location
+Init-Logs $Root_Location
 
 Write-Log "Set Execution Policies and Unblock powershell scripts" -Level DEBUG
 Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned
